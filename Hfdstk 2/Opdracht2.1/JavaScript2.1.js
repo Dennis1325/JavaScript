822ec4bdda33fa0a3d890e7b01b35774b06f4445
dennis <dennis1325@live.nl>
2017-12-07 12:47:35 +0100

    Hoofdstuk 2 gemaakt

---
+++ Hfdstk 2/Opdracht2.1/JavaScript2.1.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("leeuw").src = "../../img/Leeuw1.jpg";
+document.getElementById("wolf").src = "../../img/Wolf1.jpg";
---
+++ Hfdstk 2/Opdracht2.2/Opdracht2.2.js
@@ -0,0 +1,21 @@
+//Blokje 1
+
+document.getElementById("bl1").style.backgroundColor = "#22FFFF";
+document.getElementById("bl1").style.paddingTop = "10px";
+document.getElementById("bl1").style.paddingLeft = "20px";
+
+//Blokje 2
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "10px";
+document.getElementById("bl2").style.paddingLeft = "20px";
+
+//Blokje 3
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "25px";
+document.getElementById("bl3").style.textAlign = "right";
+
+//Blokje 4
+
+document.getElementById("bl4").style.backgroundColor = "red";
